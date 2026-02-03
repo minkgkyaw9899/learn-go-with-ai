35a5eeee224245acdfe4d4407990a580b80c0493
Min Kaung <minkgkyaw9899@gmail.com>
2026-02-03 23:25:21 +0800

    feat: learned 01_introduction_and_structure_of_go_program

---
+++ 01_introduction_and_structure_of_go_program/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Example 1: Classic "Hello World"
+	fmt.Println("Hello World")
+
+	// Example 2: Printing Multiple Lines
+	fmt.Println("Welcome to Go Course")
+	fmt.Println("let's write some code")
+
+	// Example 3: Simple Integers
+	fmt.Println("My lucky number is")
+	fmt.Println(7)
+	fmt.Println("Current Year", 2026)
+
+	// Exercise 1: The Bio Write a program that prints your Name, Age, and Country on three separate lines.
+	fmt.Println("Name: Mg Mg")
+	fmt.Println("Age: 25")
+	fmt.Println("Country: Myanmar")
+
+	// Exercise 2: The Math Printer Write a program that prints the result of 10 + 10. (Hint: Do not use quote marks "" around the math equation, or it will print as text!).
+	fmt.Println(10 + 10)
+}
