4cf7e8448a6d1180749489446989bc71e9689258
Min Kaung <minkgkyaw9899@gmail.com>
2026-02-04 08:38:51 +0800

    feat: sperate 2AI model

---
+++ claude/01_hello_world_and_basic_structure/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+	fmt.Println("Welcome to the world of Go!")
+	fmt.Println("I'm excited to learn Go!")
+
+	fmt.Print("This is Print. ")
+	fmt.Print("It doesn't add a newline.")
+	fmt.Println("But this one does!")
+
+	// Exercises 1: Write a program that prints your name and age on separate lines
+	fmt.Println("Julian")
+	fmt.Println("24")
+
+	// Exercises 2: Create a program that prints a simple ASCII art (like a smiley face) using multiple print statements
+	fmt.Println("  ^  ")
+	fmt.Println(" / ")
+	fmt.Println("(_ _)")
+	fmt.Println("  v  ")
+
+	// Exercises 3: Write a program that prints "Go" 5 times, each on a new line
+	fmt.Println("Go")
+	fmt.Println("Go")
+	fmt.Println("Go")
+	fmt.Println("Go")
+	fmt.Println("Go")
+}
